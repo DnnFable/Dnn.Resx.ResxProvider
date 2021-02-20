e1ca37f58069e6fcb805073e8f8e91cd3712c003
Stefan Cullmann <stefan.cullmann@gmail.com>
2021-02-20 19:00:47 +0100

    Version 0.1.0

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dnn.Resx.ResxProvider [![Nuget](https://img.shields.io/nuget/v/Dnn.Resx.ResxProvider?style=flat-square)](https://www.nuget.org/packages/Dnn.Resx.ResxProvider/)
+
+A F# Type Provider designed for Fable. It provides typed information on DNN's Resource Files (.resx) during design time and can be loaded with localized texts on runtime with the help of [Dnn.Resx](https://github.com/DnnFable/Dnn.Resx)
+
+## Usage
+* Install [Dnn.Resx](https://github.com/DnnFable/) on your DNN server
+* Install the package `Dnn.Resx.ResxProvider` from NuGet
+* Add a resource file and generate your model
+
+
+```fsharp
+open Browser.Dom
+
+//ViewResources is the typed representation of the resources fpr localization
+type ViewResources = Dnn.Resx.ResxProvider<"/App_LocalResources/View.resx">
+
+//it can be loaded on runtime using DNN.Resx
+Dnn.Resx.loadResources "~desktopmodules/MVC/vendor.namespaces/App_LocalResources/View.resx"
+|> Promise.map ViewResources
+|> Promise.iter
+    (fun res ->
+        // During desingn time, the items are available as properties
+        console.log res.``Input.Text``)
+```
+
+## Inspiration
+This type provider was inspired and originally forked from the  [Fable.JsonProvider](https://github.com/fable-compiler/Fable.JsonProvider)
---
+++ src/ResxProvider.fs
@@ -0,0 +1,174 @@
+namespace Dnn
+
+module Resx =
+
+    open System
+    open System.IO
+    open System.Net
+    open System.Text.RegularExpressions
+    open FSharp.Quotations
+    open FSharp.Core.CompilerServices
+    open ProviderImplementation.ProvidedTypes
+
+    open ProviderDsl
+    open Fable.Core
+
+    [<Emit("""
+    (() => {
+      const res =$0;
+      return $.get($.ServicesFramework(0).getServiceRoot('dnn.resx')+'service/get?strategy=0&resource='+res).then((data) => {return JSON.stringify(data);});
+    })()""" )>]
+    let loadResources (resxUrl: string): JS.Promise<string> = (unbox) obj ()
+
+    [<Emit("JSON.parse($0)")>]
+    let jsonParse (json: string) = obj ()
+
+    [<Emit("$0[$1]")>]
+    let getProp (o: obj) (k: string) = obj ()
+
+    let fetchUrlAsync (url: string) =
+        async {
+            let req = WebRequest.CreateHttp(url)
+
+            req.AutomaticDecompression <-
+                DecompressionMethods.GZip
+                ||| DecompressionMethods.Deflate
+
+            use! resp = req.AsyncGetResponse()
+            use stream = resp.GetResponseStream()
+            use reader = new IO.StreamReader(stream)
+            return reader.ReadToEnd()
+        }
+
+    let firstToUpper (s: string) = s.[0..0].ToUpper() + s.[1..]
+
+    let getterCode name =
+        fun (args: Expr list) -> <@@ getProp %%args.Head name @@>
+
+    let rec makeType typeName json =
+        match json with
+        | JsonParser.Null -> Any
+        | JsonParser.Bool _ -> Bool
+        | JsonParser.Number _ -> Float
+        | JsonParser.String _ -> String
+        | JsonParser.Array items ->
+            match items with
+            | [] -> Array Any
+            // TODO: Check if all items have same type
+            | item :: _ -> makeType typeName item |> Array
+        | JsonParser.Object members ->
+            let members =
+                members |> List.collect (makeMember typeName)
+
+            makeCustomType (typeName, members) |> Custom
+
+    and makeMember ns (name, json) =
+        let t = makeType (firstToUpper name) json
+
+        let m =
+            Property(name, t, false, getterCode name)
+
+        match t with
+        | Custom t -> [ ChildType t; m ]
+        | _ -> [ m ]
+
+    let parseJson asm ns typeName sample =
+        let makeRootType withCons basicMembers =
+            makeRootType (
+                asm,
+                ns,
+                typeName,
+                [ yield! basicMembers |> List.collect (makeMember "")
+                  if withCons then
+                      yield Constructor([ "json", String ], (fun args -> <@@ jsonParse %%args.Head @@>)) ]
+            )
+
+        match JsonParser.parse sample with
+        | Some (JsonParser.Object members) -> makeRootType true members |> Some
+        | Some (JsonParser.Array ((JsonParser.Object members) :: _)) ->
+            let t = makeRootType false members
+            let array = t.MakeArrayType() |> Custom
+
+            [ Method("ParseArray", [ "json", String ], array, true, (fun args -> <@@ jsonParse %%args.Head @@>)) ]
+            |> addMembers t
+
+            Some t
+        | _ -> None
+
+    [<TypeProvider>]
+    type public Resx(config: TypeProviderConfig) as this =
+        inherit TypeProviderForNamespaces(config)
+
+        let asm =
+            System.Reflection.Assembly.GetExecutingAssembly()
+
+        let ns = "Dnn.Resx"
+
+        let staticParams =
+            [ ProvidedStaticParameter("sample", typeof<string>) ]
+
+        let generator =
+            ProvidedTypeDefinition(asm, ns, "ResxProvider", Some typeof<obj>, isErased = true)
+
+        do
+            generator.DefineStaticParameters(
+                parameters = staticParams,
+                instantiationFunction =
+                    (fun typeName pVals ->
+                        match pVals with
+                        | [| :? string as arg |] ->
+                            let arg = arg.Trim()
+
+                            if Regex.IsMatch(arg, "^https?://") then
+                                async {
+                                    let! res = fetchUrlAsync arg
+
+                                    return
+                                        match parseJson asm ns typeName res with
+                                        | Some t -> t
+                                        | None -> failwithf "Response from URL %s is not a valid JSON: %s" arg res
+                                }
+                                |> Async.RunSynchronously
+                            else
+                                let readfile arg =
+                                    let filepath =
+                                        if Path.IsPathRooted arg then
+                                            arg
+                                        else
+                                            Path.GetFullPath(Path.Combine(config.ResolutionFolder, arg))
+
+                                    File.ReadAllText(filepath, System.Text.Encoding.UTF8)
+
+                                let parseResx text =
+                                    let matches =
+                                        Regex.Matches(text, "<!--(?:.|\n)*?-->|data\s+name=\"(.*?)\"")
+
+                                    seq { for m in matches -> m }
+                                    |> Seq.map (fun m -> m.Groups.[1].Value)
+                                    |> Seq.filter (System.String.IsNullOrWhiteSpace >> not)
+
+                                let content =
+                                    // Check if the string is a JSON literal
+                                    if arg.StartsWith("{") || arg.StartsWith("[") then
+                                        arg
+                                    else if arg.EndsWith(".resx") then
+                                        readfile arg
+                                        |> parseResx
+                                        |> Seq.map (fun n -> sprintf "\"%s\":\"\"," n)
+                                        |> Seq.fold (+) ""
+                                        |> sprintf "{%s}"
+
+                                    else
+                                        readfile arg
+
+
+                                match parseJson asm ns typeName content with
+                                | Some t -> t
+                                | None -> failwithf "Local sample is not a valid JSON: %s" content
+                        | _ -> failwith "unexpected parameter values")
+            )
+
+        do this.AddNamespace(ns, [ generator ])
+
+    [<assembly: TypeProviderAssembly>]
+    do ()
